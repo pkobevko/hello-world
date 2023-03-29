6e482ecb3cd0cde338b54ead300fc4638a613993
Petro Kobevko <kobevkopi@gmail.com>
2023-03-29 15:08:16 +0200

    change hello message

--- src/main/java/academy/mate/App.java
+++ src/main/java/academy/mate/App.java
@@ -2,6 +2,6 @@ package academy.mate;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello mates!");
     }
 }
